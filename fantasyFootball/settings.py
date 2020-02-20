439779517d8a3d309ed8847b14958f8837d33d0d
Brandon D <duncanb@spu.edu>
2020-02-19 20:04:12 -0800

    oh yeah got some stuff going. Menu added. Figuring stuff out.

--- fantasyFootball/settings.py
+++ fantasyFootball/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'fantasyFootball.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['fantasyFootball/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
