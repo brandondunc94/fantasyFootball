2f73e7729de5ce3106a7d8394bbff780bf77980a
Brandon Duncan - CHQ <brandon.duncan2@expeditors.com>
2020-02-27 14:33:30 -0800

    Users are now connected to leagues!

--- league/views.py
+++ league/views.py
@@ -10,15 +10,14 @@ def createLeague(request):
         form = NewLeagueForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data['name'])
+
             #Create new league model in db
             newLeague = League.objects.create(name=form.cleaned_data['name'])
 
-            #Get profile for current user
-            currentProfile = User.profile
             #Assign current user to new league - FIX THIS
-            leagueMembership = LeagueMembership(user=User,league=newLeague)
+            leagueMembership = LeagueMembership.objects.create(user=request.user,league=newLeague)
             #Return to home page
-            return render(request, '/home/')
+            return render(request, 'home/home.html')
     elif request.method == "GET":
             form = NewLeagueForm()
             return render(request, 'league/createLeague.html', {'form': form})
