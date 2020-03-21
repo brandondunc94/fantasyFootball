b146f9c2a1e1dafd2c338099bc15fb0d72943856
Brandon D <duncanb@spu.edu>
2020-03-21 11:27:49 -0700

    Changing db to postgres db on live server

--- fantasyFootball/settings.py
+++ fantasyFootball/settings.py
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'fantasyFootball.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'urban_prod',
+        'USER': 'u_urban',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
