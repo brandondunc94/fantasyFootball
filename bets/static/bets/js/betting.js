a5756fb56054862e1419ea4ee32ff252c544eee9
brandondunc94 <brandon.douglas.duncan@gmail.com>
2020-04-22 14:27:27 -0700

    Added functionality to increase/decrease bet amounts on bet page basedon user's point total.

--- bets/static/bets/js/betting.js
+++ bets/static/bets/js/betting.js
@@ -1,23 +1,16 @@
-/*Search through leagues*/
-/*$(document).ready(function() {
-    $("#searchLeagues").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#leagueTable tbody tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});*/
-
-/*Increment bet amount*/
-$(".increase-bet-button").click(function() {
-
-});
-
 /*Send a request to join a league*/
 $(".bet-box").click(function() {
     if ($(this).hasClass('bet-box-selected')) {
-        /*do nothing since the div is already selected*/
-    } else {
+        /*Unselect the currently selected box and unselect radio button*/
+        $(this).removeClass('bet-box-selected')
+        var radioButton = $(this).find("input");
+        radioButton.attr('checked', false);
+
+        /*Select opposite div and radio button to mark as untouched bet box*/
+        var oppositeDiv = $(this).siblings('.bet-box');
+        oppositeDiv.removeClass('bet-box-unselected')
+    } 
+    else {
         /*Select child radio button and div to mark as checked*/
         $(this).toggleClass(function() {
             return $(this).is('.bet-box-selected, .bet-box-unselected') ? 'bet-box-selected bet-box-unselected' : 'bet-box-selected';
@@ -33,51 +26,47 @@ $(".bet-box").click(function() {
         oppositeRadioButton = oppositeDiv.find("input");
         oppositeRadioButton.attr('checked', false);
     }
-
-
-
-
-
-
-    /*$.ajax({
-        url: '/league/request/',
-        data: {
-            'leagueName': leagueName
-        },
-        dataType: 'json',
-        success: function(data) {
-            if (data.status == "SUCCESS") {
-                requestButtonObject.prop("disabled", true);
-                requestButtonObject.prop('value', 'Requested');
-            } else if (data.status == "DUPLICATE") {
-                alert("You have already requested to join this league.");
-            } else {
-                alert("Unable to complete your request.");
-            }
-        }
-    });*/
 });
 
+/*Increase bet amount*/
+$(".increase-bet-button").click(function() {
+    /*Get total points available to bet*/
+    var totalUserPoints =  parseInt($('#totalPoints').html());
+    var pointsAvailable = totalUserPoints;
+    /*Sum up all points that have already been bet on other games*/
+    $('.game-bet-amount').each(function( index ) {
+        pointsAvailable -= (parseInt($(this).html()));
+      });
 
-/*Join league*/
-$(".join-button").click(function() {
-    var leagueName = $(this).parent().siblings('.league-name').html();
-    var joinButtonObject = $(this);
-    $.ajax({
-        url: '/league/addPublic/',
-        data: {
-            'leagueName': leagueName
-        },
-        dataType: 'json',
-        success: function(data) {
-            if (data.status == "SUCCESS") {
-                joinButtonObject.prop("disabled", true);
-                joinButtonObject.prop('value', 'Joined');
-            } else if (data.status == "DUPLICATE") {
-                alert("You are already a member of this league.");
-            } else {
-                alert("Unable to complete your request.");
-            }
-        }
-    });
+    if(pointsAvailable >= 10){
+        /*Add 10 points to current game bet*/
+        var gameBetAmountDisplay = $(this).siblings('.game-bet-amount');
+        var gameBetAmount = parseInt(gameBetAmountDisplay.html());
+        gameBetAmount += 10;
+        gameBetAmountDisplay.html(gameBetAmount.toString());
+
+        /*Subtract 10 points from points available to bet*/
+        var pointTotalDisplay = $('#pointsToBet');
+        var totalPointsAvailable = parseInt(pointTotalDisplay.html());
+        totalPointsAvailable -= 10;
+        pointTotalDisplay.html( totalPointsAvailable.toString());
+    }
 });
+
+/*Decrease bet amount*/
+$(".decrease-bet-button").click(function() {
+    /*Decrease 10 points to current game bet*/
+    var gameBetAmountDisplay = $(this).siblings('.game-bet-amount');
+    var gameBetAmount = parseInt(gameBetAmountDisplay.html());
+    if (gameBetAmount > 0){
+        gameBetAmount -= 10;
+        gameBetAmountDisplay.html(gameBetAmount.toString());
+
+        /*Add back 10 points from points available to bet*/
+        var pointTotalDisplay = $('#pointsToBet');
+        var totalPointsAvailable = parseInt(pointTotalDisplay.html());
+        totalPointsAvailable += 10;
+        pointTotalDisplay.html( totalPointsAvailable.toString());
+    }
+});
+
