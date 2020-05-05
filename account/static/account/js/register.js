1b5449264c44fe98be1a685cb459bcfa7ac6f168
brandondunc94 <brandon.douglas.duncan@gmail.com>
2020-05-05 14:13:23 -0700

    Enforced password of at least 8 characters

--- account/static/account/js/register.js
+++ account/static/account/js/register.js
@@ -1,13 +1,19 @@
-/*When passwords are entered, check if it matches the other password*/
-$(document).ready(function() {
-    $("#password1, #password2").keyup(checkPasswordMatch);
-});
+/*When password fields are entered, check if it matches the other password
+and that the length is at least 8 characters*/
+
+$("#password").keyup(checkPasswordMatch);
+$("#password2").keyup(checkPasswordMatch);
 
 function checkPasswordMatch() {
     var password = $("#password").val();
     var confirmPassword = $("#password2").val();
 
-    if (password != confirmPassword) {
+    if (password.length < 8) {
+        $("#divCheckPasswordMatch").removeClass('alert-success');
+        $("#divCheckPasswordMatch").addClass('alert-danger');
+        $('#divCheckPasswordMatch').html('Password must be at least 8 characters.');
+        $('#submitButton').prop("disabled", true);
+    } else if (password != confirmPassword) {
         $("#divCheckPasswordMatch").removeClass('alert-success');
         $("#divCheckPasswordMatch").addClass('alert-danger');
         $('#divCheckPasswordMatch').html('Passwords do not match.');
