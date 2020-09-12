239073aaf8618b5fa0834f14b161d5b2ce8a4061
brandondunc94 <brandon.douglas.duncan@gmail.com>
2020-09-12 13:23:37 -0700

    New task to save weekly scores. this will run every Tuesday morning

--- command/tasks.py
+++ command/tasks.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
-from league.models import Season, Week, Game
+from league.models import Season, Week, Game, LeagueMembership
 from datetime import datetime, timedelta
 
 @shared_task
@@ -23,3 +23,19 @@ def lockPicks():
         print("Locking an upcoming game: " + game.homeTeam.name + " VS " + game.awayTeam.name)
         
     return True
+
+@shared_task
+def saveWeeklyScores():
+    try:
+        #Get current active season
+        activeSeason = Season.objects.get(active=True)
+    except:
+        #Season has not been created, just quit this task
+        return True
+
+    #Get all league membership objects
+    allLeagueMemberships = LeagueMembership.objects.all()
+
+    for currentMembership in allLeagueMemberships:
+        currentMembership.weeklyScores = currentMembership.weeklyScores + str(currentMembership.score) + ','
+        currentMembership.save()
