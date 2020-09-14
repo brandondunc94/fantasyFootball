9380249f8fb61800ec8da72cd8fc82fba27dc288
brandondunc94 <brandon.douglas.duncan@gmail.com>
2020-09-13 20:46:46 -0700

    Adding about url

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.dashboard, name='home'),
     path("<int:weekId>/", views.dashboard, name='home'),
     path("<int:weekId>/<leagueName>/", views.dashboard, name='home'),
-    path('welcome/', views.welcome, name='welcome')
+    path('welcome/', views.welcome, name='welcome'),
+    path('about/', views.about, name='about')
 ]
