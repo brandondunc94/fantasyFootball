8be956fda12901ef1eab3d8efbf5e38953610821
brandondunc94 <brandon.douglas.duncan@gmail.com>
2020-09-19 13:15:18 -0700

    Added favorite team to user profile model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from league.models import League
+from league.models import League, Team
 
 class Profile(models.Model):
     user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
@@ -8,4 +8,5 @@ class Profile(models.Model):
     firstName = models.TextField(max_length=50, blank=True)
     lastName = models.TextField(max_length=50, blank=True)
     timezone = models.CharField(max_length=50, blank=True, default="US/Pacific")
+    favoriteTeam = models.ForeignKey(Team, on_delete=models.SET_NULL, default=None, null=True)
     lastPageAccessed = models.TextField(max_length=20, blank=True, default="League")
