0a3fcf6758abafbce82e59ba61eeb65392d40403
brandondunc94 <brandon.douglas.duncan@gmail.com>
2021-02-16 13:22:30 -0800

    Disabling account deletion now that season is over and the site has a visitor option to get a basic idea of how it works

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,7 @@ from account import views
 urlpatterns = [
     path('', views.account_page, name='Account Home'),
     path('create/', views.create_account, name='New Account'),
-    path('delete/', views.delete_account, name='Delete Account'),
+    #path('delete/', views.delete_account, name='Delete Account'),
     path('lastAccess/', views.update_last_accessed_page, name='AJAX CALL - Update last accessed page'),
     path('update/', views.update_profile, name='AJAX CALL - Update profile when user clicks save'),
     path('<username>/',views.public_account_page, name='Public Account View'),
